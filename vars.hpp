0547decf9e2f4cb2f1a488d93bd6d92570797121
mju-oss-13-d <mjuoss13d@gmail.com>
2013-12-18 01:52:18 +0900

    Create vars.hpp

---
+++ vars.hpp
@@ -0,0 +1,28 @@
+#define VM_N 30
+#define VM_M 20
+#define VM_Scale 25
+
+#define VD_W VM_N * VM_Scale
+#define VD_H VM_M * VM_Scale
+
+
+
+
+
+struct snk {
+	int x;
+	int y;
+};
+
+
+
+
+int dir = 1, sh = 5;
+
+snk s[100];
+
+
+int step = 300;
+
+
+int MX, myMX;
